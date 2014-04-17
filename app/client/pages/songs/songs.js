80c3da78e6db463833ab5d474fb29614f50ee850
Abhilash Goje <me@goje87.com>
2014-04-17 16:02:20 +0530

    Made the song upload call based out of promise

--- app/client/pages/songs/songs.js
+++ app/client/pages/songs/songs.js
@@ -1,8 +1,52 @@
 app.defineComponent({
   name: 'songs',
   api: (function() {
-    var uploadInfo = new Reactive(),
-        req = null;
+    var uploadInfo = new Reactive();
+
+    function uploadSong(file) {
+      var d = Q.defer(),
+          formData = new FormData(),
+          req = new XMLHttpRequest();
+
+      formData.append('f', file);
+
+      req.upload.addEventListener('progress', function(event) {
+        var percent = parseInt((event.loaded*100)/event.total),
+            status = 'uploading';
+
+        uploadInfo.write({status: status, percent: percent});
+      });
+
+      req.addEventListener('load', function(event) {
+        var response = JSON.parse(event.target.responseText);
+
+        if(!response.success) {
+          uploadInfo.write({status: 'idle'});
+          d.reject('There was an error while storing the file on server');
+          return;
+        }
+
+        uploadInfo.write({status: 'idle'});
+        d.resolve(response);
+      });
+
+      req.addEventListener('abort', function() {
+        uploadInfo.write({status: 'idle'});
+        d.resolve({isAborted: true});
+      });
+
+      req.addEventListener('error', function() {
+        uploadInfo.write({status: 'idle'});
+        d.reject('There was as error while uploading');
+      });
+
+      var userId = app.ask('user')._id;
+
+      req.open('POST', 'http://gdrive.goje87.com/upload?user='+userId);
+      req.send(formData);
+
+      return d.promise;
+    }
 
     return {
       data: {
@@ -37,69 +81,14 @@ app.defineComponent({
 
           var fileInput = event.currentTarget,
               file = fileInput.files[0],
-              formData = new FormData(),
-              tags, image;
-
-          req = new XMLHttpRequest();
+              tags, image,
+              url = file.url || file.name;
 
           // for now allow only mp3 files upload
           if(file.type !== 'audio/mp3') {
             alert('Currently only mp3 files are allowed for upload. Please try again selecting mp3 format');
             return;
           }
-
-          formData.append('f', file);
-          // formData.append('user', app.ask('user')._id);
-
-          req.upload.addEventListener('progress', function(event) {
-            var percent = parseInt((event.loaded*100)/event.total),
-                status = 'uploading';
-
-            uploadInfo.write({status: status, percent: percent});
-
-          });
-
-          req.addEventListener('load', function(event) {
-            var response = JSON.parse(event.target.responseText);
-
-            if(!response.success) {
-              uploadInfo.write({status: 'idle'});
-              alert('There was a error while storing the file on server');
-              return;
-            }
-
-            uploadInfo.write({status: 'idle'});
-            alert('Uploaded successfully!');
-
-            var obj = {
-              title: tags.title,
-              album: tags.album,
-              artist: tags.artist,
-              image: image,
-              url: response.url,
-              id3: tags,
-              filename: file.name
-            };
-
-
-            app.collection('songs').add(obj);
-          });
-
-          req.addEventListener('abort', function() {
-            uploadInfo.write({status: 'idle'});
-          });
-
-          req.addEventListener('error', function() {
-            uploadInfo.write({status: 'idle'});
-            alert('There was an error while uploading');
-          });
-
-          var userId = app.ask('user')._id;
-
-          req.open('POST', 'http://gdrive.goje87.com/upload?user='+userId);
-          req.send(formData);
-
-          var url = file.url || file.name;
 
           ID3.loadTags(url, function() {
 
@@ -116,6 +105,28 @@ app.defineComponent({
           }, {
             tags: ["title","artist","album","picture"],
             dataReader: FileAPIReader(file)
+          });
+
+          uploadSong(file)
+          .then(function(response) {
+            if(response.isAborted) return;
+
+            var obj = {
+              title: tags.title,
+              album: tags.album,
+              artist: tags.artist,
+              image: image,
+              url: response.url,
+              id3: tags,
+              filename: file.name
+            };
+
+            app.collection('songs').add(obj);
+
+            alert('Song uploaded successfully');
+          })
+          .fail(function(error) {
+            alert(error);
           });
 
         },

cf25012cbe89fe5612ecbaea7e88df817824c734
Abhilash Goje <abhilash.goje@flipkart.com>
2014-08-08 05:17:22 +0530

    Fixing minor bugs

--- app/client/pages/browse/browse.js
+++ app/client/pages/browse/browse.js
@@ -1,14 +1,14 @@
 app.defineComponent({
     name: 'browse',
     api: (function() {
-        
+
         var loadStatus = new Reactive(false);
 
         function searchQuery (q) {
-            Meteor.http.call("GET", "http://api.flipkart.com/InternalApi/QuickKart/search?q="+q,function(searchResult,error){                
-                loadStatus = true;
+            Meteor.http.call("GET", "http://api.flipkart.com/InternalApi/QuickKart/search?q="+q,function(searchResult,error){
+                loadStatus.write(true);
                 console.log(searchResult,error);
-            });           
+            });
         }
 
         return {
@@ -16,10 +16,10 @@ app.defineComponent({
                 loadStatus: function(){
                     return loadStatus.read();
                 }
-             
+
             },
             events: {
-                
+
             },
             onRender:function(){
                 var query = app.param('query');

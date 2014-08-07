8d32d1ef7fe540783b7a00f7f0621042dcf76228
Harshita Kasera <harshita.k@BLRVS-Harshita.local>
2014-08-07 20:11:41 +0530

    Removed unncessary code

--- app/router.js
+++ app/router.js
@@ -7,35 +7,7 @@
   });
 
   app.routes([
-    {name: 'home', path: '/'},
-    {name: 'browse', path: '/b/?:path(*)'},
-    {name: 'login', path: '/login'},
-    {name: 'logout', path: '/logout'},
-    {name: 'albums', path: '/albums'},
-    {name: 'newAlbum', path: '/albums/new'},
-    {name: 'album', path: '/album/:id'},
-    {name: 'songs', path: '/songs'}
+    {name: 'home', path: '/'}
   ]);
-
-  // Ask user to login before viewing any pages
-  app.on('beforePageRender', function(route) {
-    var allowedRoutes = [
-      'login', 'logout'
-    ];
-
-    // If current route is found in allowed routes, then dont do anything
-    if(_.indexOf(allowedRoutes, route.name) >= 0) return true;
-
-    // Check if user is logged in
-    app.action('callServerMethod', 'getUserId', function(err, userId) {
-      if(err) throw err;
-
-      if(!userId) {
-        app.action('redirect', 'login');
-      }
-    });
-
-    return true;
-  });
 
 })(this);

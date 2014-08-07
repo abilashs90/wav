9dc7beee9dd2fcf56d93251e4e4851c1196dbb6d
Harshita Kasera <harshita.k@BLRVS-Harshita.local>
2014-08-08 02:55:03 +0530

    Added sample rating

--- app/client/pages/home/home.js
+++ app/client/pages/home/home.js
@@ -5,7 +5,7 @@ app.defineComponent({
             inputStatus = new Reactive('is-not-listening');
 
         function searchQuery (q) {
-            Meteor.http.call("GET", "http://172.17.88.254/InternalApi/QuickKart/search?q="+q,function(searchResult,error){
+            Meteor.http.call("GET", "http://api.flipkart.com/InternalApi/QuickKart/search?q="+q,function(searchResult,error){
                 console.log(searchResult,error);
             });           
         }

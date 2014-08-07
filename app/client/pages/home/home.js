3c3394f6bd42a18808ece2098bb6dfa265bfe00c
Harshita Kasera <harshita.k@BLRVS-Harshita.local>
2014-08-08 01:44:08 +0530

    Added search query api

--- app/client/pages/home/home.js
+++ app/client/pages/home/home.js
@@ -4,6 +4,12 @@ app.defineComponent({
         var inputType = new Reactive('is-voice-input'),
             inputStatus = new Reactive('is-not-listening');
 
+        function searchQuery (q) {
+            Meteor.http.call("GET", "http://172.17.88.254/InternalApi/QuickKart/search?q="+q,function(searchResult,error){
+                console.log(searchResult,error);
+            });           
+        }
+
         return {
             data: {
               inputType: function() {
@@ -20,6 +26,8 @@ app.defineComponent({
                     // $(".p-input-big-keyboard").removeClass("invisible");
                     app.ask('startListning', function (text) {
                         this.$(".search-text").val(text);
+                        searchQuery(text);
+
                     });
                 },
                 'click #footer-search-action': function () {

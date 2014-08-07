ac786c40268cab9135162ddd663852acb1381764
abilash.s <abilash.s.90@gmail.com>
2014-08-08 03:20:02 +0530

    More on text box

--- app/client/pages/home/home.js
+++ app/client/pages/home/home.js
@@ -2,12 +2,13 @@ app.defineComponent({
     name: 'home',
     api: (function() {
         var inputType = new Reactive('is-voice-input'),
-            inputStatus = new Reactive('is-not-listening');
+            inputStatus = new Reactive('is-not-listening'),
+            inputText = new Reactive();
 
         function searchQuery (q) {
-            Meteor.http.call("GET", "http://172.17.88.254/InternalApi/QuickKart/search?q="+q,function(searchResult,error){
-                console.log(searchResult,error);
-            });           
+            // Meteor.http.call("GET", "http://172.17.88.254/InternalApi/QuickKart/search?q="+q,function(searchResult,error){
+            //     console.log(searchResult,error);
+            // });           
         }
 
         return {
@@ -17,17 +18,25 @@ app.defineComponent({
               },
               inputStatus: function() {
                 return inputStatus.read();
+              },
+              inputText: function() {
+                return inputText.read();
               }
             },
             events: {
                 'click .p-home-icon-cont': function() {
+                    inputText.write("");
                     inputType.write('is-voice-input');
                     inputStatus.write('is-listening');
                     app.ask('startListning', function (text) {
                         inputStatus.write('is-finished-listening');
-                        this.$(".search-text").val(text);
+                        // this.$(".search-text").val(text);
+                        inputText.write(text);
                         searchQuery(text);
-
+                    }, function (text) {
+                        // this.$(".search-text").val(text);
+                        inputText.write(text);
+                        searchQuery(text);                        
                     });
                 },
                 'click #footer-search-action': function () {

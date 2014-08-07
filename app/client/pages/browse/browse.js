e3a987936256b2c6ecb41f51c7c92a9d4a1585b1
abilash.s <abilash.s.90@gmail.com>
2014-08-08 05:15:22 +0530

    initial browse page changes

--- app/client/pages/browse/browse.js
+++ app/client/pages/browse/browse.js
@@ -3,10 +3,11 @@ app.defineComponent({
     api: (function() {
         
         var loadStatus = new Reactive(false);
+        var query = new Reactive();
 
         function searchQuery (q) {
             Meteor.http.call("GET", "http://api.flipkart.com/InternalApi/QuickKart/search?q="+q,function(searchResult,error){                
-                loadStatus = true;
+                loadStatus.write(true);
                 console.log(searchResult,error);
             });           
         }
@@ -15,16 +16,19 @@ app.defineComponent({
             data: {
                 loadStatus: function(){
                     return loadStatus.read();
+                },
+                query: function () {
+                    return query.read();
                 }
              
             },
             events: {
-                
+                                
             },
             onRender:function(){
-                var query = app.param('query');
-                searchQuery(query);
-
+                var q = app.param('query');
+                searchQuery(q);
+                query.write(q);
             }
         };
     })()

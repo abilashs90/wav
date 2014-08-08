45bea150dd862522a480cf2ed46ff257e1cc47a9
Harshita Kasera <harshita.k@BLRVS-Harshita.local>
2014-08-08 11:11:45 +0530

    Added browse list category selection

--- app/client/pages/browse/browse.js
+++ app/client/pages/browse/browse.js
@@ -6,12 +6,22 @@ app.defineComponent({
         var query = new Reactive();
         var browseProducts = new Reactive();
         var matchedCategories = new Reactive();
+        var loadListStatus = new Reactive(false);
 
+        function searchUrl(url){
+            mainUrl = "http://api.flipkart.com"+url;
+            Meteor.http.call("GET",mainUrl ,function(error,searchResult){
+                loadListStatus.write(true);            
+                browseProducts.write(searchResult.data.RESPONSE.data.products);             
+            });
+        }
         function searchQuery (q) {
-            Meteor.http.call("GET", "http://api.flipkart.com/InternalApi/QuickKart/search?q="+q,function(error,searchResult){
-                loadStatus.write(true);                            
+            var url = "http://api.flipkart.com/InternalApi/QuickKart/search?q="+q;
+            Meteor.http.call("GET",url ,function(error,searchResult){
+                loadStatus.write(true);  
+                loadListStatus.write(true);                          
                 browseProducts.write(searchResult.data.RESPONSE.data.products);
-                matchedCategories.write(searchResult.data.RESPONSE.data.categories);                
+                matchedCategories.write(searchResult.data.RESPONSE.data.categories);              
             });
         }
 
@@ -28,11 +38,25 @@ app.defineComponent({
                 },
                 matchedCategories: function () {
                     return matchedCategories.read();
+                },
+                currentCategory:function(){
+                    return currentCategory.read();
+                },
+                loadListStatus:function(){
+                    return loadListStatus.read();
                 }
             },
             events: {
                 'click .p-browse-item': function(event) {
                     itemId = event.currentTarget.id;                                        
+                },
+                'click .p-browse-category':function(event){
+                    loadListStatus.write(false);
+                    $currTar = $(event.currentTarget);
+                    url = $currTar.attr("data-url");
+                    $(".p-active-category").removeClass("p-active-category");
+                    $currTar.addClass("p-active-category");
+                    searchUrl(url);
                 }
             },
             onRender:function(){
